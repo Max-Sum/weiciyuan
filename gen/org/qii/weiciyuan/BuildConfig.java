844679b3a603566d16a706eb8676db3feaa5bac6
Max Sum <maxsumprc@gmail.com>
2013-10-26 23:37:17 +0800

    remove stuff

--- gen/org/qii/weiciyuan/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package org.qii.weiciyuan;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
